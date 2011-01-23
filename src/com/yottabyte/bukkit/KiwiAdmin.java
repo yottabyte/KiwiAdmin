ce903df710550bb428e5ceb03eb5ee0e3c4da08f
yottabyte <yottabyte@hotmail.se>
2011-01-23 15:55:01 +0100

    Initial commit.

---
+++ src/com/yottabyte/bukkit/KiwiAdmin.java
@@ -0,0 +1,110 @@
+
+package com.yottabyte.bukkit;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bukkit.Server;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginLoader;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import com.nijikokun.bukkit.Permissions.Permissions;
+
+/**
+ * Chat plugin
+ *
+ * @author yottabyte
+ */
+
+public class KiwiAdmin extends JavaPlugin {
+		
+	public static final Logger log = Logger.getLogger("Minecraft");
+	
+	static Permissions CurrentPermissions = null;
+	
+	public static Map<String,Boolean> bannedPlayers = new HashMap<String,Boolean>();
+    private final KiwiAdminPlayerListener playerListener = new KiwiAdminPlayerListener(this);
+
+    public KiwiAdmin(PluginLoader pluginLoader, Server instance,
+            PluginDescriptionFile desc, File folder, File plugin,
+            ClassLoader cLoader) {
+        super(pluginLoader, instance, desc, folder, plugin, cLoader);
+    }
+        // NOTE: Event registration should be done in onEnable not here as all events are unregistered when a plugin is disabled
+
+	public void setupPermissions() {
+		Plugin plugin = this.getServer().getPluginManager().getPlugin("Permissions");
+
+		if (KiwiAdmin.CurrentPermissions == null) {
+			// Permission plugin already registered
+			return;
+		}
+		
+		if (plugin != null) {
+			KiwiAdmin.CurrentPermissions = (Permissions) plugin;
+		} else {
+			log.log(Level.CONFIG, "Permissions plugin is required for this plugin to work. Disabling plugin");
+			this.getServer().getPluginManager().disablePlugin(this);
+		}
+	}
+    
+    public void onDisable() {
+        // NOTE: All registered events are automatically unregistered when a plugin is disabled
+
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+        System.out.println("Goodbye world! KiwiAdmin is going to sleep! :(");
+    }
+
+    public void onEnable() {
+        // Register our events   	
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvent(Event.Type.PLAYER_COMMAND, playerListener, Priority.Normal, this);
+        pm.registerEvent(Event.Type.PLAYER_JOIN, playerListener, Priority.High, this);
+
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+        PluginDescriptionFile pdfFile = this.getDescription();
+        System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
+        
+        
+		try {
+			
+			File banlist = new File("plugins/KiwiAdmin/banlist.txt");
+			
+			if (banlist.exists()){ 
+	            BufferedReader in = new BufferedReader(new FileReader(banlist));
+	            String data = null;
+	               
+	            while ((data = in.readLine()) != null){
+	            		//Checking for blank lines
+	            		if (data.length()>0){
+	            			bannedPlayers.put(data, true);
+	            		}
+	            		
+	                }
+	            in.close();
+	            }else{
+	        	    File file = new File("plugins/KiwiAdmin/banlist.txt");
+	                if (!file.exists()) {
+	                  new File("plugins/KiwiAdmin").mkdir();
+	                  file.createNewFile();
+	                  System.out.println("Banlist not found, creating banlist.txt!");
+	                }
+	            }
+			}
+        catch (IOException e) {
+
+            e.printStackTrace();
+            
+            }
+        
+    }
+}
---
+++ src/com/yottabyte/bukkit/KiwiAdminPlayerListener.java
@@ -0,0 +1,111 @@
+
+package com.yottabyte.bukkit;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.entity.Player;
+
+import com.nijikokun.bukkit.Permissions.Permissions;
+
+/**
+ * Simple admin plugin for Bukkit.
+ * @author yottabyte
+ */
+public class KiwiAdminPlayerListener extends PlayerListener {
+    private final KiwiAdmin plugin;
+    public KiwiAdminPlayerListener(KiwiAdmin instance) {
+        plugin = instance;
+    }
+    
+    public static String combineSplit(int startIndex, String[] string, String seperator) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = startIndex; i < string.length; i++) {
+            builder.append(string[i]);
+            builder.append(seperator);
+        }
+
+        builder.deleteCharAt(builder.length() - seperator.length()); // remove
+        return builder.toString();
+    }
+   public void banPlayer(String name){
+	   KiwiAdmin.bannedPlayers.put(name, true);
+		try
+        {
+        	BufferedWriter banlist = new BufferedWriter(new FileWriter("plugins/KiwiAdmin/banlist.txt",true));
+            banlist.newLine();                    
+            banlist.write(name);
+            banlist.close();
+        }
+        catch(IOException e)          
+        {
+        	System.out.println("KiwiAdmin: Couldn't write to banlist.txt");
+        }
+   }
+	    	
+    @Override
+    public void onPlayerCommand(PlayerChatEvent event) {
+        Player player = event.getPlayer();
+        String fullMsg[] = event.getMessage().split(" ");
+        
+        if(fullMsg[0].equalsIgnoreCase("/kick")){
+        	if (Permissions.Security.permission(event.getPlayer(), "kiwiadmin.kick")) {
+        		if (fullMsg.length > 1) {
+	        		String p = fullMsg[1];
+	        		Player victim = plugin.getServer().getPlayer(p);
+	        		if(victim != null){
+	        			if(fullMsg.length < 3){
+	        				victim.kickPlayer("You have been kicked by " + player.getName() + ".");
+	        			}else{
+	        				String reason = combineSplit(2, fullMsg, " ");
+	        				victim.kickPlayer("You have been kicked by " + player.getName() + ". Reason: " + reason);
+	        			}
+	        		}else{
+	        			player.sendMessage("§cKick failed: " + p + " isn't online.");
+	        		}
+        		}else{
+        			player.sendMessage("§eUsage: /kick [player] (reason)");
+        		}
+        	}
+        }
+	     if(fullMsg[0].equalsIgnoreCase("/ban")){
+	    	if (Permissions.Security.permission(event.getPlayer(), "kiwiadmin.ban")) {
+	    		
+	    		if (fullMsg.length > 1) {
+	        		String p = fullMsg[1];
+	        		Player victim = plugin.getServer().getPlayer(p); // What player is really the victim?
+	        		
+	        		banPlayer(p); // Add him to banned-players.txt!
+	        		
+	        		if(victim != null){ // If he is online, kick him with a nice message :)
+	        			
+	        			if(fullMsg.length < 3){ //No reason, just kick.
+	        				victim.kickPlayer("You have been banned by " + player.getName() + ".");
+	        			}else{ // Look at that, a reason! Good admin :)
+	        				String reason = combineSplit(2, fullMsg, " ");
+	        				victim.kickPlayer("You have been banned by " + player.getName() + ". Reason: " + reason);
+	        			}
+	        		}
+	        		player.sendMessage("§aBanned §4" + p + "§a!"); // Let the banner know that he succeeded.
+	    		}else{
+	    			player.sendMessage("§eUsage: /ban [player] (reason)");
+	    		}
+	    	}
+	    }
+    }
+    public void onPlayerJoin(PlayerEvent event){
+    	Player player = event.getPlayer();
+    	Boolean banned = KiwiAdmin.bannedPlayers.get(player.getName());
+    	if(banned != null){
+    		player.kickPlayer("You are banned from this server.");
+    		System.out.println(player.getName() + " is banned! Deny");
+    	}else{
+    		System.out.println(player.getName() + " is not banned. Allow");
+    	}
+    }
+}
